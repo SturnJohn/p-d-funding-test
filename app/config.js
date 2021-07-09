209dfc5e08ac8ff769823066fdd9db89d357fef6
John <john.sturniolo@gmail.com>
2021-07-09 16:55:47 +0100

    About this Fund
    Created page layout

--- app/config.js
+++ app/config.js
@@ -4,7 +4,7 @@
 
 module.exports = {
   // Service name used in header. Eg: 'Renew your passport'
-  serviceName: 'Service name goes here',
+  serviceName: 'Test Fund landing page',
 
   // Default port that prototype runs on
   port: '3000',
